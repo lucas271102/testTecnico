eb51ff22a93025b52f376ee8c50bab5726549c24
lucas271102 <lucas001spe@gmail.com>
2023-08-04 17:54:26 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Navbar from './components/Navbar'
+import { Outlet } from 'react-router-dom'
 function App() {
 
 
   return (
     <>
-    <h1 className='text-red-500'>Hoal</h1>
+    <Navbar/>
+    <Outlet/>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-blue-900 w-[100%] p-4 flex'>
+     <div className='flex gap-6'>
+        <h2 className='text-white'>+549-165269434</h2>
+        <h2 className='text-white'>info@vetly.com.ar</h2>
+     </div>
+    </div>
+  )
+}
+
+export default Navbar
